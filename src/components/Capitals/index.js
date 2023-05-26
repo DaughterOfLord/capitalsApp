8f52d2465ebeb6bbe85987fd994991e8efc8de43
deepika <deepikadasari78@gmail.com>
2023-05-26 05:37:39 +0000

    updated capitalsApp

--- src/components/Capitals/index.js
+++ src/components/Capitals/index.js
@@ -29,6 +29,61 @@ const countryAndCapitalsList = [
     capitalDisplayText: 'Helsinki',
     country: 'Finland',
   },
+  {
+    id: 'OTTAWA',
+    capitalDisplayText: 'Ottawa',
+    country: 'Canada',
+  },
+  {
+    id: 'SOFIA',
+    capitalDisplayText: 'Sofia',
+    country: 'Bulgaria',
+  },
+  {
+    id: 'CAIRO',
+    capitalDisplayText: 'Cairo',
+    country: 'Egypt',
+  },
+  {
+    id: 'ROME',
+    capitalDisplayText: 'Rome',
+    country: 'Italy',
+  },
+  {
+    id: 'JERUSALEM',
+    capitalDisplayText: 'Jerusalem',
+    country: 'Israel',
+  },
+  {
+    id: 'JAKARTA',
+    capitalDisplayText: 'Jakarta',
+    country: 'Indonesia',
+  },
+  {
+    id: 'MALE',
+    capitalDisplayText: 'Malé',
+    country: 'Maldives',
+  },
+  {
+    id: 'KUALA LUMPUR',
+    capitalDisplayText: 'Kuala Lumpur',
+    country: 'Malaysia',
+  },
+  {
+    id: 'ABUJA',
+    capitalDisplayText: 'Abuja',
+    country: 'Nigeria',
+  },
+  {
+    id: 'ISLAMABAD',
+    capitalDisplayText: 'Islamabad',
+    country: 'Pakistan',
+  },
+  {
+    id: 'MOSCOW',
+    capitalDisplayText: 'Moscow',
+    country: 'Russia',
+  },
 ]
 
 // Write your code here
@@ -57,25 +112,27 @@ class Capitals extends Component {
     const country = this.getCountry(activeCapitalId)
 
     return (
-      <div className="bg-container">
-        <div className="card-container">
+      <div className="app-container">
+        <div className="capitals-container">
           <h1 className="heading">Countries And Capitals</h1>
-          <select
-            className="capital-select"
-            onClick={this.onChangeCapital}
-            value={activeCapitalId}
-          >
-            {countryAndCapitalsList.map(eachCapital => {
-              ;<option
-                key={eachCapital.id}
-                value={eachCapital.id}
-                className="option"
-              >
-                {eachCapital.capitalDisplayText}
-              </option>
-            })}
-          </select>
-          <p className="question">is capital of which country?</p>
+          <div className="question-container">
+            <select
+              className="capital-select"
+              onChange={this.onChangeCapital}
+              value={activeCapitalId}
+            >
+              {countryAndCapitalsList.map(eachCapital => (
+                <option
+                  key={eachCapital.id}
+                  value={eachCapital.id}
+                  className="option"
+                >
+                  {eachCapital.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="question">is capital of which country?</p>
+          </div>
           <p className="country">{country}</p>
         </div>
       </div>
